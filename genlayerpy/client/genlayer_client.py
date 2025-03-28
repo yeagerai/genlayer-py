2d4adb89f77b8449f91e77cdb55336d5a652a4a8
Daniel Rojas <danielrojasc888@gmail.com>
2025-03-28 16:06:26 -0500

    feat: add account actions

--- genlayerpy/client/genlayer_client.py
+++ genlayerpy/client/genlayer_client.py
@@ -3,8 +3,15 @@ from typing import Optional
 from genlayerpy.types import Chain
 from genlayerpy.provider import GenLayerProvider
 from web3 import Web3
-from typing import Optional
+from web3.types import Nonce, BlockIdentifier, ENS
+from hexbytes import HexBytes
+from eth_typing import (
+    Address,
+    ChecksumAddress,
+)
+from typing import Optional, Union
 from eth_account.signers.local import LocalAccount
+from genlayerpy.accounts.actions import get_current_nonce, fund_account
 
 
 class GenLayerClient(Eth):
@@ -19,3 +26,16 @@ class GenLayerClient(Eth):
         self.provider = GenLayerProvider(url)
         web3 = Web3(provider=self.provider)
         super().__init__(web3)
+
+    ## Account actions
+    def fund_account(
+        self, address: Union[Address, ChecksumAddress, ENS], amount: int
+    ) -> HexBytes:
+        return fund_account(self, address, amount)
+
+    def get_current_nonce(
+        self,
+        address: Optional[Union[Address, ChecksumAddress, ENS]] = None,
+        block_identifier: Optional[BlockIdentifier] = None,
+    ) -> Nonce:
+        return get_current_nonce(self, address, block_identifier)

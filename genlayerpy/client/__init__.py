ffe55e5451e6ec654ac2ecd0477888d8a74c8534
Daniel Rojas <danielrojasc888@gmail.com>
2025-03-28 10:08:44 -0500

    refactor: separate genlayer_client from client to avoid circular import in future imports

--- genlayerpy/client/__init__.py
+++ genlayerpy/client/__init__.py
@@ -1,1 +1,2 @@
+from .genlayer_client import GenLayerClient
 from .client import create_client
--- genlayerpy/client/client.py
+++ genlayerpy/client/client.py
@@ -1,21 +1,7 @@
-from web3.eth import Eth
 from typing import Optional
 from genlayerpy.types import Chain
 from genlayerpy.chains import localnet
-from genlayerpy.provider import GenLayerProvider
-from web3 import Web3
-
-
-class GenLayerClient(Eth):
-    """
-    The client to interact with GenLayer Network
-    """
-
-    def __init__(self, chain_config: Chain, account: Optional[str] = None):
-        url = chain_config.rpc_urls["default"]["http"][0]
-        self.provider = GenLayerProvider(url)
-        web3 = Web3(provider=self.provider)
-        super().__init__(web3)
+from .genlayer_client import GenLayerClient
 
 
 def create_client(

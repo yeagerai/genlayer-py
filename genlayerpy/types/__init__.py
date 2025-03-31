df02d3f25bbfcf5b7bd0710a8ec8df82710ef827
Daniel Rojas <danielrojasc888@gmail.com>
2025-03-31 18:20:17 -0500

    feat: add contract actions

--- genlayerpy/types/__init__.py
+++ genlayerpy/types/__init__.py
@@ -1,3 +1,4 @@
 from .calldata import CalldataAddress, CalldataEncodable
 from .transactions import GenLayerTransaction, TransactionStatus
 from .chain import Chain, NativeCurrency, ConsensusMainContract, SimulatorChain
+from .contracts import ContractSchema

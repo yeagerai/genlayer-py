b59cffccb0eb65e5077c85843c8cfb6eca164d1e
Daniel Rojas <danielrojasc888@gmail.com>
2025-05-20 14:27:19 -0500

    feat: add e2e tests

---
+++ tests/e2e/storage.py
@@ -0,0 +1,22 @@
+# { "Depends": "py-genlayer:test" }
+
+from genlayer import *
+
+
+# contract class
+class Storage(gl.Contract):
+    storage: str
+
+    # constructor
+    def __init__(self, initial_storage: str):
+        self.storage = initial_storage
+
+    # read methods must be annotated with view
+    @gl.public.view
+    def get_storage(self) -> str:
+        return self.storage
+
+    # write method
+    @gl.public.write
+    def update_storage(self, new_storage: str) -> None:
+        self.storage = new_storage

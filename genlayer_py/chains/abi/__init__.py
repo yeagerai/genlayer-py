8aba8fddab061aeccab483e0a87a288a0d6975b9
Daniel Rojas <danielrojasc888@gmail.com>
2025-05-27 17:00:27 -0500

    style: apply black

--- genlayer_py/chains/abi/__init__.py
+++ genlayer_py/chains/abi/__init__.py
@@ -1,8 +1,12 @@
 import json
 import importlib.resources
 
-consensus_data_abi_path = importlib.resources.files("genlayer_py").joinpath("chains/abi/consensus_data_abi.json")
-consensus_main_abi_path = importlib.resources.files("genlayer_py").joinpath("chains/abi/consensus_main_abi.json")
+consensus_data_abi_path = importlib.resources.files("genlayer_py").joinpath(
+    "chains/abi/consensus_data_abi.json"
+)
+consensus_main_abi_path = importlib.resources.files("genlayer_py").joinpath(
+    "chains/abi/consensus_main_abi.json"
+)
 
 with open(consensus_data_abi_path, "r") as f:
     CONSENSUS_DATA_ABI = json.load(f)

855aa64ffdd30f66bc31d19b958550d04fe509bf
Daniel Rojas <danielrojasc888@gmail.com>
2025-06-12 15:49:23 -0500

    feat: implement consensus module

--- genlayer_py/abi/transactions.py
+++ genlayer_py/abi/transactions.py
@@ -2,6 +2,11 @@ import rlp
 from eth_utils import to_hex
 
 
+def deserialize(data):
+    deserialized_data = rlp.decode(data)
+    return deserialized_data
+
+
 def serialize(data):
     serialized_data = rlp.encode(data)
     return to_hex(serialized_data)
